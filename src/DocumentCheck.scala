f79dba88e14a454775495a51ecb0d1e73e8583c9
Andrew Mueller <ajm5774@gmail.com>
2013-01-31 21:12:41 -0500

    Added DocumentCheck

---
+++ src/DocumentCheck.scala
@@ -0,0 +1,26 @@
+import akka.actor.ActorRef
+import akka.actor.Actor
+import scala.util.Random
+import scala.Array
+import scala.Int
+
+class DocumentCheck (val queues : Array[ActorRef]) extends Actor{
+	private final val FAILURE_RATE = 20
+	private var inc = 1
+  
+  def receive = {
+
+    case(name : String)=>
+      println(name + "Arrived at the document check")
+	  val rand = new Random()
+	  if(rand.nextInt(99)+1 > FAILURE_RATE){
+		println(name + "Passed the document check")
+	    queues(inc) ! name
+	    inc += 1
+	    inc %= queues.length
+	  }
+	  else{
+	    println(name + "Failed the document check")	  
+	  }
+  }
+}
